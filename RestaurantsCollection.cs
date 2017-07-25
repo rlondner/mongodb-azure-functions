eb473c4ca6353b9d37122c27d2473e5477dcf131
rlondner <raphael@londner.net>
2017-07-24 18:45:17 -0700

    removed unused using references

--- RestaurantsCollection.cs
+++ RestaurantsCollection.cs
@@ -1,10 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MongoDB.Tutorials.AzureFunctions
 {
